7ee983e0b6ab6cc9c2d074b53f19a89339d5ce22
kishoreg245 <53132280+kishoreg245@users.noreply.github.com>
2019-07-21 13:25:40 +1000

    Fixed an error

--- Helloworld.ps1
+++ Helloworld.ps1
@@ -1,7 +1,7 @@
 # My first file - PS1
 # ========================================
-#WELCOME
+#.     WELCOME
 #
-==========================================
+#==========================================
 write-output "Hello world"
 write-output "First PS1 on Github"
